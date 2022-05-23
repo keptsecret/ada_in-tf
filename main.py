014b04d7ec0cc300cba4de66b8c8921462a156a6
keptsecret <sorchon@gmail.com>
2022-05-23 16:01:03 +0700

    minor change to args

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def train(content_dir, style_dir):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('action', metavar='(train)/(infer)', type=str)
+    parser.add_argument('action', metavar="train/infer", type=str)
     parser.add_argument('content_dir', metavar='content_directory', type=str, help="path to directory of content images")
     parser.add_argument('style_dir', metavar='style_directory', type=str, help="path to directory of style images")
     parser.add_argument('--alpha', type=float, help="strength of style transfer (only on inference)")
