7561ac5981e1ac9b369e0894928d851a30224e89
keptsecret <sorchon@gmail.com>
2022-05-25 23:03:32 +0700

    implement infer, adjust params

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import tensorflow.keras as K
 from PIL import Image
 
 from train import Trainer
+from model import StyleNet
 
 UP_SIZE = (1024, 1024)
 
 def train(content_dir, style_dir):
-    trainer = Trainer(content_dir, style_dir, batch_size=64, num_iter=8e3, lr=1e-4)
+    trainer = Trainer(content_dir, style_dir, batch_size=64, num_iter=15000, lr=5e-5)
     trainer.train()
 
 def infer(content_dir, style_dir, model_path, alpha):
@@ -31,12 +32,15 @@ def infer(content_dir, style_dir, model_path, alpha):
     content_img = tf.image.resize(content_img, UP_SIZE)
     style_img = tf.image.resize(style_img, UP_SIZE)
 
-    model = K.models.load_model(model_path)
+    model = StyleNet()
+    r_img = tf.random.normal(content_img.shape)
+    _, _ = model(dict(content_imgs=r_img, style_imgs=r_img, alpha=alpha))
+    model.load_weights(model_path)
     print(model.summary())
 
     stylized_img, _ = model(dict(content_imgs=content_img, style_imgs=style_img, alpha=alpha))
     stylized_img = tf.image.resize(stylized_img, [w, h])
-    K.preprocessing.image.save_img("image.png", stylized_img[0])
+    K.utils.save_img("image.png", stylized_img[0], scale=True)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
